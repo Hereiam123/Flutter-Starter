9f092417494f5b02f9e0647f40040a7cb3c839d5
Brian De Maio <bdemaio1@gmail.com>
2019-12-27 18:54:24 -0500

    Adding in a saved word list to favorite word pairs

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
   Widget _buildList() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
@@ -34,7 +35,10 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
-    return ListTile(title: Text(pair.asPascalCase));
+    final alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+        trailing: Icon(alreadyDaved ? Icons.favorite : Icons.block));
   }
 
   Widget build(BuildContext content) {
